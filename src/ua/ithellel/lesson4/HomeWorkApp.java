06175af00c8f56dfb4af1bb9848e5bc08a9a6453
macbook <andrii.krasikov@gmail.com>
2022-08-11 16:58:41 +0300

    POPITKA #5

--- src/ua/ithellel/lesson4/HomeWorkApp.java
+++ src/ua/ithellel/lesson4/HomeWorkApp.java
@@ -3,21 +3,21 @@ package ua.ithellel.lesson4;
 //1. Создать пустой проект в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод main().
 public class HomeWorkApp {
     public static void main(String[] args) {
-        printThreeWords();                       //1
-        checkSumSign(2, 3);                //2
-        printColor(2);                     //3
-        compareNumber(2, 3);               //4
-        boolNumbersSum(2, 3);              //5
-        checkNumb(1);                         //6
-        boolNumb1(2);                         //7
-        stringXTimes("text", 5);           //8
-        checkYear2(2022);                     //9
+        printThreeWords();                       //2
+        checkSumSign(2, 3);                //3
+        printColor(2);                     //4
+        compareNumber(2, 3);               //5
+        System.out.println(boolNumbersSum(2, 3));              //6
+        checkNumb(1);                         //7
+        System.out.println(boolNumb1(2));                         //8
+        stringXTimes("text", 5);           //9
+        System.out.println(checkYear2(2022));                     //10
 
     }
 
     // 2. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
     public static void printThreeWords() {
-        System.out.println("                  TASK-1");
+        System.out.println("                  TASK-2");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
@@ -25,22 +25,28 @@ public class HomeWorkApp {
 
     //  3. Создайте метод checkSumSign()
     public static void checkSumSign(int a, int b) {
-        System.out.println("                  TASK-2");
-        if (a + b >= 0) {
+        int c = a + b;
+        System.out.println("                  TASK-3");
+        if (c >= 0) {
             System.out.println("Сумма положительная");
-        } else if (a + b < 0) {
+        } else {
             System.out.println("Сумма отрицательная");
         }
     }
+
     // 4. Создайте метод printColor()
     public static void printColor(int value) {
         System.out.println("                  TASK-4");
         if (value <= 0) {
             System.out.println("Красный");
         } else if (value > 0 && value <= 100) {
-         System.out.println("Зеленый");
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
         }
     }
+
+
 // 5. Создайте метод compareNumbers(),
 
     public static void compareNumber(int a, int b) {
@@ -48,18 +54,18 @@ public class HomeWorkApp {
         if (a >= b) {
             System.out.println("a >= b");
         } else {
-        System.out.println("a < b");
-    }}
+            System.out.println("a < b");
+        }
+    }
 
 // 6. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
 
-    public static void boolNumbersSum(int a, int b) {
+    public static boolean boolNumbersSum(int a, int b) {
+        int c = a + b;
         System.out.println("                  TASK-6");
-        if (a + b >= 10 && a + b <= 20) {
-            System.out.println("true");
-        } else {
-        System.out.println("false");
-    }}
+        return (c >= 10 && c <= 20);
+
+    }
 
 //    7. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
 
@@ -71,12 +77,10 @@ public class HomeWorkApp {
     }
 
     //8. Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
-    public static void boolNumb1(int a) {
+    public static boolean boolNumb1(int a) {
         System.out.println("                  TASK-8");
-        if (a < 0) {
-            System.out.println("true");
-        } else System.out.println("false");
-    }
+            return (a < 0);
+        }
 
 
     //9. Написать метод, которому в качестве аргументов передается строка и число, метод должен отпечатать в консоль указанную строку, указанное количество раз;
@@ -90,13 +94,10 @@ public class HomeWorkApp {
 
 
     //      10. * Написать метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true, не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-    public static void checkYear2(int a) {
+    public static boolean checkYear2(int a) {
         System.out.println("                  TASK-10");
-        if (((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0)) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
+        return (((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0));
         }
     }
-}
+
 
