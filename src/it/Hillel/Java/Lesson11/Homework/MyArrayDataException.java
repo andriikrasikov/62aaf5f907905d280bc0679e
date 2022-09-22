11754cbd4d5509af049dcd57151c954a93c193ac
macbook <andrii.krasikov@gmail.com>
2022-09-22 15:10:23 +0300

    homework10

---
+++ src/it/Hillel/Java/Lesson11/Homework/MyArrayDataException.java
@@ -0,0 +1,12 @@
+package it.Hillel.Java.Lesson11.Homework;
+
+public class MyArrayDataException extends RuntimeException{
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
