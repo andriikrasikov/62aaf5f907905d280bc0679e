334bf3ce1a6c4d827a5ab380c6ca3df0d4102f57
macbook <andrii.krasikov@gmail.com>
2022-10-21 14:50:57 +0300

    Lesson13

---
+++ out/src/ua/ithillel/lesson13/homework/Orange.java
@@ -0,0 +1,11 @@
+package ua.ithillel.lesson13.homework;
+
+public class Orange implements Fruit {
+
+    private static final double WEIGHT = 1.5D;
+
+    @Override
+    public double getWeight() {
+        return WEIGHT;
+    }
+}
